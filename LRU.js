9511027f019fdaa7a3147416827747ee335672aa
夜静听雨 <7710500+mao0826@user.noreply.gitee.com>
2022-12-14 21:30:01 +0800

    LRU

--- LRU.js
+++ LRU.js
@@ -11,7 +11,9 @@ var LRUCache = function (capacity) {
   this.capacity = capacity;
   this.map = new Map();
   this.head = new DLinkedList(-1, -1, null, null);
-  this.tail = new DLinkedList(-1, -1, null, null);
+  this.tail = new DLinkedList(-1, -1, this.head, null);
+  this.head.next = this.tail;
+  this.size = 0;
 };
 
 /**
@@ -21,22 +23,7 @@ var LRUCache = function (capacity) {
 LRUCache.prototype.get = function (key) {
   const node = this.map.get(key);
   if (node) {
-    // 被访问 移动到头结点
-    const headNext = this.head.next;
-    if (node !== this.tail.prev) {
-      const nodePrev = node.prev;
-      const nodeNext = node.next;
-      if (nodePrev) nodePrev.next = nodeNext;
-      if (nodeNext) nodeNext.prev = nodePrev;
-    } else {
-      const nodePrev = node.prev;
-      nodePrev.next = this.tail;
-      this.tail.prev = nodePrev;
-    }
-    this.head.next = node;
-    if (headNext) node.next = headNext.next;
-    if (headNext) headNext.prev = node;
-    node.prev = this.head;
+    this.moveToHead(node);
     return node.value;
   }
   return -1;
@@ -50,27 +37,47 @@ LRUCache.prototype.get = function (key) {
 LRUCache.prototype.put = function (key, value) {
   let node = this.map.get(key);
   if (node) {
-    // 存在key对应的值了
+    // 存在key对应的值了 移动到头结点
+    node.value = value;
+    this.moveToHead(node);
   } else {
     node = new DLinkedList(key, value, null, null);
-    if (this.head.next === null) {
-      this.head.next = node;
-      this.tail.prev = node;
-      node.prev = this.head;
-      node.next = this.tail;
-    }
-    const headNext = this.head.next;
-    this.head.next = node;
-    node.prev = this.head;
-    node.next = headNext;
-    headNext.prev = node;
     this.map.set(key, node);
+    this.addToHead(node);
+    if (++this.size > this.capacity) {
+      // 删除最后一个节点
+      const tail = this.removeTail();
+      this.map.delete(tail.key);
+      this.size--;
+    }
   }
 };
+LRUCache.prototype.addToHead = function (node) {
+  node.prev = this.head;
+  node.next = this.head.next;
+  this.head.next.prev = node;
+  this.head.next = node;
+};
+LRUCache.prototype.removeNode = function (node) {
+  node.prev.next = node.next;
+  node.next.prev = node.prev;
+};
+LRUCache.prototype.moveToHead = function (node) {
+  this.removeNode(node);
+  this.addToHead(node);
+};
+LRUCache.prototype.removeTail = function () {
+  const res = this.tail.prev;
+  this.removeNode(res);
+  return res;
+};
 
-const lru = new LRUCache(10);
-lru.put(1, 10);
-lru.put(2, 20);
-// console.log(lru.get(1));
-console.log(lru.tail.prev);
-console.log(lru.head.next)
+const lru = new LRUCache(1);
+lru.put(2, 1);
+console.log(lru.get(2));
+lru.put(3, 2);
+console.log(lru.get(2));
+console.log(lru.get(3));
+// [[1],[2,1],[2],[3,2],[2],[3]]
+console.log(lru.tail.prev.value);
+console.log(lru.head.next.value);
